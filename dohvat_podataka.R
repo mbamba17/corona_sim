128c818228c17ef418dd96f4790b0d8d7d7234a7
Mario Bambulovic <mario.bambulovic17@gmail.com>
2020-03-31 20:08:54 +0200

    finiširanje skripte za dohvat poduzeća i simulaciju

--- dohvat_podataka.R
+++ dohvat_podataka.R
@@ -32,6 +32,9 @@ ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_area() + boje_fill
 # u %
 pom <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="SCA" & na_item=="B1G" & unit=="PC_TOT" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,sektor=nace_r2,values) %>% filter(sektor!="TOTAL") %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum)
 ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_col() + boje_fill
+# usporedba po zemljama
+pom <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="SCA" & na_item=="B1G" & unit=="PC_TOT") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,geo,sektor=nace_r2,values) %>% filter(sektor!="TOTAL" & datum==max(datum)) %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum,-geo)
+ggplot(pom,aes(x=geo,y=values,fill=sektor)) + geom_col() + boje_fill
 
 # 2. Plaće po djelatnostima ####
 
@@ -41,8 +44,11 @@ ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_area() + boje_fill
 # u %
 pom <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="SCA" & na_item=="D11" & unit=="PC_TOT" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,sektor=nace_r2,values) %>% filter(sektor!="TOTAL") %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum)
 ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_col() + boje_fill
+# usporedba po zemljama
+pom <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="SCA" & na_item=="D11" & unit=="PC_TOT") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,geo,sektor=nace_r2,values) %>% filter(sektor!="TOTAL" & datum==max(datum)) %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum,-geo)
+ggplot(pom,aes(x=geo,y=values,fill=sektor)) + geom_col() + boje_fill
 
-# 3. Broj zaposlenih
+# 3. Broj zaposlenih ####
 
 # u 000 osoba
 pom <- get_eurostat(id="namq_10_a10_e") %>% filter(s_adj=="SCA" & na_item=="EMP_DC" & unit=="THS_PER" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,sektor=nace_r2,values) %>% filter(sektor!="TOTAL") %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum)
@@ -50,7 +56,45 @@ ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_area() + boje_fill
 # u %
 pom <- get_eurostat(id="namq_10_a10_e") %>% filter(s_adj=="SCA" & na_item=="EMP_DC" & unit=="PC_TOT_PER" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,sektor=nace_r2,values) %>% filter(sektor!="TOTAL") %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum)
 ggplot(pom,aes(x=datum,y=values,fill=sektor)) + geom_area() + boje_fill
+# usporedba po zemljama
+pom <- get_eurostat(id="namq_10_a10_e") %>% filter(s_adj=="SCA" & na_item=="EMP_DC" & unit=="PC_TOT_PER") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,geo,sektor=nace_r2,values) %>% filter(sektor!="TOTAL" & datum==max(datum)) %>% mutate(sektor=case_when(sektor=="A"~"Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~"Industrija i rudarstvo",sektor=="C"~"Prerađivačka industrija",sektor=="F"~"Građevinarstvo",sektor=="G-I"~"Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"Informacije i komunikacije",sektor=="K"~"Financijske djelatnosti",sektor=="L"~"Poslovanje nekretninama",sektor=="M_N"~"Stručne i administrativne djelatnosti",sektor=="O-Q"~"Javna uprava",sektor=="R-U"~"Ostale uslužne djelatnosti")) %>% spread(sektor,values) %>% mutate(`Industrija i rudarstvo`=`Industrija i rudarstvo`-`Prerađivačka industrija`) %>% gather(key = sektor,value = values,-datum,-geo)
+ggplot(pom,aes(x=geo,y=values,fill=sektor)) + geom_col() + boje_fill
+
+# 4. Potrošnja po vrstama dobara ####
+pom <- get_eurostat(id="namq_10_fcs") %>% filter(s_adj=="SCA" & unit=="CP_MNAC" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,na_item,values) %>% mutate(vrsta=case_when(na_item=="P311_S14"~"Trajna dobra",na_item=="P312N_S14"~"Ostala dobra i usluge",T~as.character(na_item))) %>% select(-na_item) %>% filter(vrsta!="P31_S14")
+ggplot(pom,aes(x=datum,y=values,fill=vrsta)) + geom_area() + boje_fill
+# usporedba po zemljama - ovo treba prilagoditi jer naši nisu unijeli sva polja 
+pom <- get_eurostat(id="namq_10_fcs") %>% filter(s_adj=="SCA" & unit=="CP_MNAC") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% filter(datum==max(datum)) %>% select(datum,geo,na_item,values) %>% mutate(vrsta=case_when(na_item=="P311_S14"~"Trajna dobra",na_item=="P312N_S14"~"Ostala dobra i usluge",T~as.character(na_item))) %>% select(-na_item) %>% filter(vrsta!="P31_S14")
+ggplot(pom,aes(x=geo,y=values,fill=vrsta)) + geom_col(position="fill") + boje_fill
+
+# 5. Annual enterprise statistics ####
+podaci <- get_eurostat(id="sbs_sc_sca_r2") %>% filter(geo=="HR" & size_emp!="TOTAL") %>% mutate(size_emp=case_when(size_emp=="0-9"~"Micro",size_emp=="10-19"~"Small",size_emp=="20-49"~"Small",size_emp=="50-249"~"Medium",size_emp=="GE250"~"Large"))
 
 
 
-pom <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="NSCA" & na_item=="B1G" & unit=="CP_MNAC" & geo=="HR" & nace_r2=="TOTAL") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,values)
+################################# Poduzeća za DZS-a ############################
+
+# dohvat i populacija sifrarnika - treba učitati šifrarnik i nadopuniti rupe
+library(readxl)
+library(zoo)
+sfr <- read_excel(path = "poduzeca.xlsx",sheet = "sifrarnik")
+sfr$razina1 <- na.locf(sfr$razina1)
+sfr$razina2[-1] <- na.locf(sfr$razina2[-1])
+sfr$razina3[-c(1,2)] <- na.locf(sfr$razina3[-c(1,2)])
+# sad je on popunio sve pa treba ubaciti neke NA-ove kada prelazi iz jedne kategorije u drugu
+for(i in 2:nrow(sfr)){
+  if(sfr$razina1[i] != sfr$razina1[i-1]){
+    sfr$razina2[i] <- NA
+    sfr$razina3[i] <- NA
+  }
+}
+for(i in 3:nrow(sfr)){
+  if(is.na(sfr$razina2[i-1])){
+    sfr$razina3[i] <- NA
+  } else if(is.na(sfr$razina2[i])){
+    sfr$razina3[i] <- NA
+  } else if (sfr$razina2[i] != sfr$razina2[i-1]){
+    sfr$razina3[i] <- NA
+  }
+}
+# sad ćemo brojčanim oznakama u razinama pridružiti opisne oznake
--- nkd_podaci.R
+++ nkd_podaci.R
@@ -2,6 +2,7 @@ library(readxl)
 library(zoo)
 library(tidyverse)
 library(lubridate)
+library(eurostat)
 
 # Ovi podaci se skidaju sa DZS -> Strukturne poslovne statistike
 
@@ -37,7 +38,40 @@ temp2 <- read_excel(path = "poduzeca.xlsx", sheet = "f") %>% gather(key = "varij
 temp3 <- read_excel(path = "poduzeca.xlsx", sheet = "g") %>% gather(key = "varijabla",value = "iznos",-djelatnost,-godina)
 temp4 <- read_excel(path = "poduzeca.xlsx", sheet = "hijlmns") %>% gather(key = "varijabla",value = "iznos",-djelatnost,-godina)
 temp5 <- read_excel(path = "poduzeca.xlsx", sheet = "k") %>% gather(key = "varijabla",value = "iznos",-djelatnost,-godina)
-poduzeca=rbind(temp1,temp2,temp3,temp4,temp5)
+poduzeca=rbind(temp1,temp2,temp3,temp4,temp5) %>% mutate(iznos=ifelse(iznos=="z",NA,ifelse(iznos=="-",NA,iznos))) %>% mutate(iznos=as.numeric(iznos))
 # sklapanje sa šiframa
-poduzeca <- full_join(sfr,poduzeca,by="djelatnost")
+poduzeca <- left_join(poduzeca,sfr,by="djelatnost") %>% na.omit()
 rm(temp1,temp2,temp3,temp4,temp5,sfr,i,pom1,pom2,pom3)
+# kraljevi su neke tablic množili sa 000, a neke nisu, pa trebamo i to uzeti u obzir
+poduzeca <- poduzeca %>% mutate(tisuce=case_when(str_detect(varijabla,"(tis. kuna)")~"da",T~"ne")) %>% mutate(iznos=ifelse(tisuce=="da",iznos*1000,iznos),varijabla=str_remove(varijabla,pattern = "\\ \\(tis.\\ kuna\\)")) %>% mutate(razina0="Poduzeća") %>% select(razina0,razina1,razina2,razina3,razina4,godina,djelatnost,nacerev2,varijabla,iznos)
+save(poduzeca,file="poduzeca.Rda")
+
+# analiza
+# plaće
+pom <- poduzeca  %>% group_by(godina) %>% filter(varijabla=="13320 Nadnice i plaće") %>% summarise(iznos=sum(iznos,na.rm=T))
+# broj_zaposlenih
+pom <- poduzeca  %>% group_by(godina) %>% filter(varijabla=="16110 Broj zaposlenih osoba") %>% summarise(iznos=sum(iznos,na.rm=T))
+# dodana vrijednost - WINNER!
+pom <- poduzeca  %>% group_by(godina) %>% filter(varijabla=="12150 Dodana vrijednost prema troškovima proizvodnih čimbenika") %>% summarise(iznos=sum(iznos,na.rm=T))
+# bruto poslovni višak
+pom <- poduzeca  %>% group_by(godina) %>% filter(varijabla=="12170 Bruto poslovni višak") %>% summarise(iznos=sum(iznos,na.rm=T))
+
+# dodana vrijednost - WINNER!
+pom <- poduzeca  %>% group_by(godina,razina1) %>% filter(varijabla=="12150 Dodana vrijednost prema troškovima proizvodnih čimbenika") %>% summarise(iznos=sum(iznos,na.rm=T)) %>% spread(godina,iznos)
+skopiraj(pom)
+
+# poslovni višak
+pom <- poduzeca  %>% group_by(godina,razina1) %>% filter(godina>=2013 & varijabla=="12110 Promet") %>% summarise(iznos=sum(iznos,na.rm=T)) %>% spread(godina,iznos)
+skopiraj(pom)
+
+# ispis BDV-a u excel za pondere
+
+pom <- poduzeca %>% group_by(razina1,razina2,razina3,razina4) %>% filter(godina=="2017" & varijabla=="12150 Dodana vrijednost prema troškovima proizvodnih čimbenika") %>% summarise(iznos=sum(iznos,na.rm=T))
+write.xlsx2(data.frame(pom),file = "radni.xlsx",append=T)
+
+# umanjenje bdp-a
+ponderi <- read_excel(path = "radni.xlsx",sheet = "umanjenje") %>% mutate(iznos1=iznos*(1+ponder)) %>% group_by(razina1) %>% summarise(iznos=sum(iznos,na.rm=T),iznos1=sum(iznos1,na.rm = T)) %>% mutate(ponder=(iznos1/iznos-1)*100)
+skopiraj(ponderi)
+
+bdv <- get_eurostat(id="namq_10_a10") %>% filter(s_adj=="NSA" & na_item=="B1G" & unit=="CP_MNAC" & geo=="HR") %>% mutate(datum = ceiling_date(time,"quarter")-1) %>% select(datum,sektor=nace_r2,values) %>% filter(sektor!="TOTAL") %>% mutate(sektor=case_when(sektor=="A"~"A. Poljoprivreda, šumarstvo i ribarstvo",sektor=="B-E"~" B,D,E. Industrija i rudarstvo",sektor=="C"~"C. Prerađivačka industrija",sektor=="F"~"F. Građevinarstvo",sektor=="G-I"~"G,H,I. Trgovina, prijevoz, smještaj, priprema hrane",sektor=="J"~"J. Informacije i komunikacije",sektor=="K"~"K. Financijske djelatnosti",sektor=="L"~"L. Poslovanje nekretninama",sektor=="M_N"~" M,N. Stručne i administrativne djelatnosti",sektor=="O-Q"~"O,P,Q. Javna uprava",sektor=="R-U"~"R,S,T,U. Ostale uslužne djelatnosti")) %>% arrange(datum,sektor) %>% spread(sektor,values) %>% mutate(` B,D,E. Industrija i rudarstvo`=` B,D,E. Industrija i rudarstvo`-`C. Prerađivačka industrija`)
+skopiraj(bdv)
